5f354cb9656be01ef712f3a9f0f86536694722f8
Shivani Maurya <shivani.shivi333@gmail.com>
2024-09-24 07:56:24 +0530

    add delete btn

--- src/components/ExpenseList/ExpenseList.jsx
+++ src/components/ExpenseList/ExpenseList.jsx
@@ -1,30 +1,26 @@
 import React from "react";
 import styles from "./ExpenseList.module.css";
+import Transaction from "../Transaction/Transaction";
 
-export default function ExpenseList({ expenses, deleteExpense }) {
+const ExpenseList = ({ expenses, deleteExpense }) => {
   return (
     <div className={styles.expenseListContainer}>
       <h3>Transactions</h3>
       <ul className={styles.transactionList}>
-        {expenses.length > 0 ? (
-          expenses.map((expense, index) => (
-            <li key={index} className={styles.transactionItem}>
-              <span>
-                {expense.text}: ${expense.amount.toFixed(2)}
-              </span>
-              <button 
-                className="delete-btn" 
-                style={{ marginLeft: '10px' }} 
-                onClick={() => deleteExpense(index)}
-              >
-                delete
-              </button>
-            </li>
-          ))
-        ) : (
-          <li>No transactions to display</li>
-        )}
+        {expenses.map((expense, i) => {
+          return (
+            <Transaction
+              index={i}
+              key={expense.id}
+              expense={expense}
+              deleteExpense={deleteExpense}
+              //Pass props here to handle updation
+            />
+          );
+        })}
       </ul>
     </div>
   );
-}
+};
+
+export default ExpenseList;
--- src/components/Transaction/Transaction.jsx
+++ src/components/Transaction/Transaction.jsx
@@ -3,51 +3,49 @@ import styles from "./Transaction.module.css";
 import EditImage from "../../images/edit.png";
 import DeleteImage from "../../images/trash-bin.png";
 
-export default function Transaction ({expense, index}) {
-  
-  const [currentIndex, setCurrentIndex] = useState(null)
-
-
-  
-    return (
-      <li
-        key={expense.id}
-        className={`${styles.transaction} ${
-          expense.amount > 0 ? styles.profit : styles.loss
-        }`}
-        onMouseOver={() => {
-          
-          setCurrentIndex(index);
-        }}
-        onMouseLeave={() => {
+const Transaction = ({ expense, deleteExpense, index }) => {
+  const [currentHoverIndex, setCurrentHoverIndex] = useState(null);
+  return (
+    <li
+      key={expense.id}
+      className={`${styles.transaction} ${
+        expense.amount > 0 ? styles.profit : styles.loss
+      }`}
+      onMouseOver={() => {
+        setCurrentHoverIndex(index);
+      }}
+      onMouseLeave={() => {
+        setCurrentHoverIndex(null);
+      }}
+    >
+      <div>{expense.text}</div>
+      <div className={styles.transactionOptions}>
+        <div
+          className={`${styles.amount} ${
+            currentHoverIndex === index && styles.movePrice
+          }`}
+        >
+          ${expense.amount}
+        </div>
+        <div
+          className={`${styles.btnContainer} ${
+            currentHoverIndex === index && styles.active
+          }`}
+        >
          
-          setCurrentIndex(null);
-        }}
-      >
-        <div>{expense.text}</div>
-        <div className={styles.transactionOptions}>
-          <div
-            className={`${styles.amount} ${
-              currentHoverIndex === index &&
-              styles.movePrice
-            }`}
-          >
-            ${expense.amount}
+          <div className={styles.edit} onClick={() => {}}>
+            <img src={EditImage} height="100%" alt="Edit" />
           </div>
           <div
-            className={`${styles.btnContainer} ${
-              currentHoverIndex === index && styles.active
-            }`}
+            className={styles.delete}
+            onClick={() => deleteExpense(expense.id)}
           >
-            <div className={styles.edit} onClick={() => {}}>
-              <img src={EditImage} height="100%" alt="Edit" />
-            </div>
-            <div className={styles.delete} onClick={() => {}}>
-              <img src={DeleteImage} height="100%" alt="Delete" />
-            </div>
+            <img src={DeleteImage} height="100%" alt="Delete" />
           </div>
         </div>
-      </li>
-    );
-  }
+      </div>
+    </li>
+  );
+};
 
+export default Transaction;
